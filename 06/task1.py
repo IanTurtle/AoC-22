5363e4a3e6fad8c00101e1359bd5f3cdfb3547ac
Ian Thompson <ianthompson1994@gmail.com>
2022-12-06 09:30:44 +0000

    day 6

---
+++ 06/task1.py
@@ -0,0 +1,17 @@
+import io
+
+answer = 4
+with open("input.txt", "rb") as input:
+	fi = io.FileIO(input.fileno())
+	fb = io.BufferedReader(fi)
+	# Read in the first 4 characters
+	check = fb.read(4)
+	
+	while char := fb.read(1):		
+		if len(check) > len(set(check)):
+			check = check[1:] + char
+			answer += 1
+		else:
+			break
+		
+print(answer)
